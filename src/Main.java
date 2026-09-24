f381b1f5642fd4ada3bbe80979b6d54ae00f8481
DainisM <moisejenko11@gmail.com>
2026-09-24 12:14:40 +0200

    Add to main to start the game

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     static void main(String[] args) {
 
+        // Build map and set players starting room
+        Adventure.buildMap();
+
+        // Start the game
+        UserInterface.runGame();
     }
 }
